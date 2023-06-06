cec418c625f8b23d2e87f1636227ce38305f38f3
Noha <nohamfarid29@gmail.com>
2023-06-06 21:09:28 +0200

    adding my art to the project

--- include.js
+++ include.js
@@ -6526,6 +6526,13 @@ let cards = [
     author: 'Marion',
     githubLink: 'https://github.com/marionjudy13'
   },
+  {
+    artName: 'NohaAnimation',
+    pageLink: './Art/NohaAnimation/index.html',
+    imageLink: './Art/NohaAnimation/NohaAnimation.gif',
+    author: 'Noha',
+    githubLink: 'https://github.com/Noha-f'
+  }
 ];
 
 // +--------------------------------------------------------------------------------+
